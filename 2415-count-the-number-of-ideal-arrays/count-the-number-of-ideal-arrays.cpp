5fd2bda26bd4e8ab97dd1e7b5f9f80ed002a2def
AMARJEET KUMAR <140300391+AmarjeetJyotis@users.noreply.github.com>
2025-04-22 11:14:34 +0530

    Time: 7 ms (72.48%) | Memory: 9.9 MB (64.22%) - LeetSync

--- 2415-count-the-number-of-ideal-arrays/count-the-number-of-ideal-arrays.cpp
+++ 2415-count-the-number-of-ideal-arrays/count-the-number-of-ideal-arrays.cpp
@@ -1,29 +1,50 @@
+const int mod = 1e9+7, maxi = 1e4+10, maxp = 15;
+int dp[maxi + maxp][maxp+1];
+vector<int> pcnt[maxi];
+int sieve[maxi];
+
 class Solution {
 public:
-    int n,m,dp[10005][20];
-    int cnt[10005][20];
-    int idealArrays(int n, int m) {
-        int mod = 1e9+7;
-        for(int i=1; i<=m; i++) {
-		    dp[i][1]=(dp[i][1]+1)%mod;
-		    for(int j=i+i; j<=m; j+=i) {
-			    for(int k=1; k<=18; k++) {
-				    dp[j][k]=(dp[j][k]+dp[i][k-1])%mod;
-			    }
-		    }
-	    }
-	    for(int i=0; i<=n; i++) {
-		    cnt[i][0]=1;
-		    for(int j=1; j<=min(i,18); j++) {
-			    cnt[i][j]=(cnt[i-1][j]+cnt[i-1][j-1])%mod;
-		    }
-	    }
-	    long long ans=0;
-	    for(int i=1; i<=m; i++) {
-		    for(int j=1; j<=18; j++) {
-			    ans=(ans+1ll*dp[i][j]*cnt[n-1][j-1]%mod)%mod;
-		    }
-	    }
-	    return ans;
+    void solve(){
+        if(dp[0][0]) return;
+        for(int i=2; i<maxi; i++){   //Mark the primes
+            if(sieve[i]==0){
+                for(int j=i; j<maxi; j+=i) sieve[j] = i;
+            }
+        }
+
+        for(int i=2; i<maxi; i++){   //Count the factors for each i
+            int curr = i;
+            while(curr > 1){
+                int p = sieve[curr];
+                int cnt = 0;
+                while(curr%p == 0){
+                    curr/=p;
+                    cnt++;
+                }
+                pcnt[i].push_back(cnt);
+            }
+        }
+
+        dp[0][0] = 1;
+        for(int i=1; i<maxi+maxp; i++){    //fill the dp table
+            dp[i][0] = 1;
+            for(int j=1; j<=min(i, maxp); j++){
+                dp[i][j] = (dp[i-1][j] + dp[i-1][j-1])%mod;
+            }
+        }
+    }
+
+    int idealArrays(int n, int maxValue) {
+        long long ans = 0;
+        solve();
+        for(int i=1; i<=maxValue; i++){
+            long long mul = 1;
+            for(int j: pcnt[i]){
+                mul = mul*dp[n+j-1][j]%mod;
+            }
+            ans = (ans+mul)%mod;
+        }
+        return ans;
     }
 };
